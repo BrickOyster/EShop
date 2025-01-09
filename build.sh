21dc40900a911219d22c34adf4cd893118ed7437
BrickOyster <dimitris2002keramidas@gmail.com>
2025-01-09 20:56:59 +0200

    add logs to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-sudo docker compose down
-echo y | sudo docker system prune
-echo y | sudo docker volume prune
-echo y | sudo docker image prune
-echo y | sudo docker builder prune
-sudo docker volume rm eshop_order_db eshop_product_db eshop_keycloak-db-data eshop_keycloak-realm-data
+sudo docker compose down > logdown
+echo y | sudo docker system prune > logprune
+echo y | sudo docker volume prune >> logprune
+echo y | sudo docker image prune >> logprune
+echo y | sudo docker builder prune >> logprune
+sudo docker volume rm eshop_order_db eshop_product_db eshop_keycloak-db-data eshop_keycloak-realm-data >> logprune
 
 sleep 2
 clear
-sudo docker compose up -d --build
+sudo docker compose up -d --build > logup
