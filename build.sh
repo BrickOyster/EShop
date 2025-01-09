871cd8984981012b8ca07050870e070d24c88c05
BrickOyster <dimitris2002keramidas@gmail.com>
2025-01-09 19:58:12 +0200

    add linux build script

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ echo y | sudo docker image prune
 echo y | sudo docker builder prune
 sudo docker volume rm eshop_order_db eshop_product_db eshop_keycloak-db-data eshop_keycloak-realm-data
 
+sleep 2
 clear
-docker compose up -d --build
+sudo docker compose up -d --build
